8bc259b665face0e81b41d4bd0b80defc7dfc759
Cosmo Ciancio <ciancio.cosmo@gmail.com>
2023-07-18 20:34:17 +0300

    1.2

---
+++ osa1/Tehtävä 1.2./App.js
@@ -0,0 +1,51 @@
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part name = {props.p1} exercises = {props.e1}/>
+      <Part name = {props.p2} exercises = {props.e2}/>
+      <Part name = {props.p3} exercises = {props.e3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
